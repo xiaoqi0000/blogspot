76e496b440b8000536de1535ec750f88ab0054d1
xiaoqi0000 <1991678106@qq.com>
2024-10-06 11:05:50 +0800

    lowdb模块化

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,17 @@ var express = require('express');
 var router = express.Router();
 const info = require('../json/info.json');
 /* GET home page. */
+
+const db = require('../lowdb/db.js');
 router.get('/', function (req, res, next) {
+  //   //获取数据
+  let num = db.get('indexNum').value();
+  // // console.log(num);
+
+  // //更改数据
+  db.set('indexNum', num + 1).write();
+
+
   res.render('index', info);
 });
 
