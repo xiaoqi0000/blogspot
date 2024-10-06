87f7962940b8f431ac39a1483684287ae84c8cbe
xiaoqi0000 <102272890+xiaoqi0000@users.noreply.github.com>
2024-10-06 10:36:09 +0800

    lowdb

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const info = require('../json/info.json');
+
+//lowdb
+const lowdb = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('../json/user.json');
+const db = lowdb(adapter);
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  //获取数据
+let num = db.get('indexNum').value();
+// console.log(num);
+
+//更改数据
+db.set('indexNum', num + 1).write();
+
+
   res.render('index', info);
 });
 
