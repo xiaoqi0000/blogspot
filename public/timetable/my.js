cb90ca57b6f106b91d2c05d5b4d4e3f4daacbd38
xiaoqi0000 <1991678106@qq.com>
2024-10-21 21:38:32 +0800

    完善课程表

---
+++ public/timetable/my.js
@@ -0,0 +1,159 @@
+//眼睛部分交互
+const eye = document.querySelector("#eye");
+
+eye.addEventListener("click", function () {
+    if (eye.src.match("open")) {
+        eye.src = eye.src.replace("open", "close");
+        //显示今天
+        const div1 = document.querySelector(".show-one");
+        const div2 = document.querySelector(".show-week");
+        div1.style.display = "block";
+        div2.style.display = "none";
+    } else {
+        eye.src = eye.src.replace("close", "open");
+        //显示本周
+        const div1 = document.querySelector(".show-one");
+        const div2 = document.querySelector(".show-week");
+        div2.style.display = "block";
+        div1.style.display = "none";
+    }
+});
+
+//更多内列表交互
+const listElements = document.querySelectorAll(".list");
+// console.log(listElements);
+listElements.forEach(element => {
+    element.addEventListener("click", function () {
+        if (element.classList.contains('active')) {
+            // console.log('元素有 active 类。');
+        } else {
+            // console.log('元素没有 active 类。');
+            element.classList.add('active');
+            listElements.forEach(otherElement => {
+                if (otherElement !== element && otherElement.classList.contains('active')) {
+                    otherElement.classList.remove('active');
+                }
+            });
+        }
+    });
+});
+
+//加载js显示课程表
+let cla = []
+
+cla = window.myData
+
+
+//计算各周的课程
+let classes = []
+for (let m = 0; m < 19; m++) {
+    let weekClass = [];
+    cla.forEach(i => {
+
+        i.classWeek.forEach(j => {
+            if (j == m) {
+                weekClass.push(i);
+            }
+        })
+    });
+    classes.push(weekClass);
+}
+console.log("classes", classes);
+
+
+//今天是第 week 周
+let weekNum = (function getWeekNumber() {
+    const currentDate = new Date();
+    const firstDayOfSeptember = new Date(2024, 8, 2);
+    const diffTime = currentDate.getTime() - firstDayOfSeptember.getTime();
+    const oneDay = 1000 * 60 * 60 * 24;
+    const daysDiff = Math.floor(diffTime / oneDay);
+    const weekDiff = Math.floor(daysDiff / 7);
+    return weekDiff + 1;
+})()
+
+// console.log("今天是第" + weekNum + "周");
+
+//加载课程表，激活对应课程
+for (let i = 0; i < listElements.length; i++) {
+    // console.log(listElements[i].innerHTML);
+    if (listElements[i].innerHTML.match(weekNum)) {
+        //激活侧边栏
+        listElements[i].classList.add('active');
+        //更改标题
+        let h3 = document.querySelector(".head h3");
+        h3.innerHTML = "第" + weekNum + "周";
+        //加载课程表
+        loadAllClass(classes[weekNum])
+        break;
+    }
+}
+//刷新课程表
+/***
+ * 加载课程表
+ * @param {*} arr 第n周的所有课程
+ */
+function loadAllClass(arr) {
+    //清除所有课程
+    let TimeString = ["<div>08:20 - 10:00</div>", "<div>10:20 - 12:00</div>", "<div>14:10 - 15:50</div>", "<div>16:10 - 17:50</div>", "<div>19:00 - 20:40</div>"]
+    for (let i = 1; i < 8; i++) {
+        let divLoad = document.querySelector(`#d${i}`);
+        divLoad.innerHTML = `<div class="card-item">
+                        <div>08:20 - 10:00</div>
+                    </div>
+                    <div class="card-item">
+                        <div>10:20 - 12:00</div>
+                    </div>
+                    <div class="card-item">
+                        <div>14:10 - 15:50</div>
+                    </div>
+                    <div class="card-item">
+                        <div>16:10 - 17:50</div>
+                    </div>
+                    <div class="card-item">
+                        <div>19:00 - 20:40</div>
+                    </div>`;
+
+    }
+
+    console.log(arr);
+    arr.forEach(i => {
+        let divLoad = document.querySelector(`#d${i.classDay}`);
+        let divChildren = divLoad.children;
+
+        divChildren[i.classTime - 1].innerHTML = `${TimeString[i.classTime - 1]}
+                        <div>《${i.className}》</div>
+                        <div>${i.classTeacher} ${i.classRoom.slice(3)}</div > `;
+
+        //背景颜色
+        let bgc = ["#108B96", "#2A6E3F", "#C0392B", "#E58E26", "#192A56"]
+        divChildren[i.classTime - 1].style.backgroundColor = bgc[i.classTime - 1];
+        divChildren[i.classTime - 1].style.color = "#fff";
+
+    });
+}
+
+//加载今天课程
+let divToday = document.querySelector("#d0")
+const today = new Date();
+let Today = today.getDay();
+if (Today == 0) {
+    Today = 7;
+}
+let divTodayLoad = document.querySelector(`#d${Today}`);
+divToday.innerHTML = divTodayLoad.innerHTML;
+
+
+//侧边栏点击事件--更改周数，加载课程表
+// console.log(listElements);
+listElements.forEach(element => {
+    element.addEventListener("click", function () {
+        console.log(element.innerHTML);
+        //加载课程表
+        loadAllClass(classes[parseInt(element.innerHTML.match(/\d+/)[0])])
+        //更改标题
+        let h3 = document.querySelector(".head h3");
+        h3.innerHTML = element.innerHTML;
+
+    });
+});
---
+++ routes/timetable.js
@@ -0,0 +1,70 @@
+let express = require('express');
+let router = express.Router();
+const db = require('../lowdb/timetable');
+
+const timetable = require('../json/timetable.json');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.render('timetable', { cla: String(JSON.stringify(timetable)) });
+});
+router.get('/admin', function (req, res, next) {
+    res.redirect('/timetable/admin.html');
+});
+router.post('/', function (req, res, next) {
+    // console.log(req.body);
+    //验证密码
+    if (req.body.code == 'sxq') {
+        let val = req.body;
+        const vv = db.get('cla').find({ "className": val.className }).value();
+        val.classID = vv.classID;
+        val.classTeacher = vv.classTeacher;
+
+        if (val.type == 'add') {
+            //增加记录
+            let data = {
+                "className": `${val.className}`,
+                "classID": `${val.classID}`,
+                "classDay": `${val.day}`,
+                "classRoom": `文华-${val.classBuliding}号${val.classType}-${val.classRoom}`,
+                "classTeacher": val.classTeacher,
+                "classWeek": [
+                    `${val.weeks}`
+                ],
+                "classTime": `${val.time}`
+            }
+            db.get('cla').push(data).write();
+        } else if (val.type == 'remove') {
+            //删除记录
+
+            let log = db.get('cla').find(cla => {
+                //依据 第几周 星期几 第几节 课程名称 来删除记录
+                return cla.classWeek.includes(`${val.weeks}`) && cla.classTime == `${val.time}` && cla.classDay == `${val.day}` && cla.className == `${val.className}`
+            }).value();
+            // console.log("更改前的数据；", log);
+
+            if (log == undefined) {
+                res.redirect('/timetable/error.html')
+                return;
+            }
+            let arr = log.classWeek
+            // console.log(arr);
+
+            arr.splice(arr.indexOf(`${val.weeks}`), 1);
+            // console.log(arr);
+            db.get('cla').find(cla => {
+                return cla.classWeek.includes(`${val.weeks}`)
+            }).assign({ classWeek: arr }).write();
+
+            // console.log("更改后的数据：", db.get('cla').find(cla => {
+            //     return cla.classTime == `${val.time}` && cla.classDay == `${val.day}` && cla.className == `${val.className}`
+            // }).value());
+
+        }
+        res.redirect('/timetable/succeed.html');
+    } else {
+        res.redirect('/timetable/error.html');
+    }
+});
+
+module.exports = router;
