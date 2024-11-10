a1794b31e6278e9bec3aeaf8291ff8a3c51cff6c
xiaoqi0000 <1991678106@qq.com>
2024-11-10 13:46:29 +0800

    完善个人网盘

--- lowdb/pan.js
+++ lowdb/pan.js
@@ -1,7 +1,8 @@
 //lowdb
+const { log } = require('console');
 const fs = require('fs');
 const folderPath = 'public/pan/resource';
-
+const axios = require('axios');
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
@@ -10,30 +11,29 @@ const db = low(adapter)
 
 //保持数据的一致性
 
-function refreshData(params) {
+async function refreshData(data) {
+    // 检查是否是123网盘
+    if (data.path.includes('https://www.123684.com')) {
+        console.log('yydsa.top文件已添加:', data.name)
 
-    fs.readdir(__dirname + '/../public/pan/resource/', (err, files) => {
-        if (err) {
-            console.error('Error reading directory:', err);
-            return;
+    } else {
+        db.get('content').remove({ name: data.name }).write();
+        console.log('URL不合法已删除文件!', data.name);
+    }
+    // 检查URL是否合法
+    try {
+        const res = await axios.get(data.jsonPath);
+        // console.log('res.data:', res.data);
+        if (res.data.success === false) {
+            db.get('content').remove({ name: data.name }).write();
+            console.log('URL不合法已删除文件!', data.name);
+        } else {
+            console.log('URL合法文件已添加:', data.name);
         }
-        // 遍历文件数组
-        let fileList = files;
+        return;
+    } catch (error) {
+        console.error(error);
+    }
 
-        // 清空mpan.json数据
-        db.get('content').remove().write()
-
-        fileList.forEach(item => {
-            //更新mpan.json数据
-
-            db.get('content').unshift({
-                name: item,
-                path: '../img/' + item
-            }).write()
-
-        });
-
-    });
 }
-
-module.exports = refreshData;
+module.exports = { refreshData, db };
--- routes/pan.js
+++ routes/pan.js
@@ -1,50 +1,42 @@
 let express = require('express');
 let router = express.Router();
 const pan = require('../json/mpan.json');
-const refreshData = require('../lowdb/pan.js');
-
-// 上传文件
-const iconv = require('iconv-lite');
-//身份验证
-const authenticate = (req, res, next) => {
-    const identity = req.query.identity;
-    console.log(identity);
-    if (identity !== 'sxq') {
-        return res.status(401).send('身份验证失败，文件已删除。');
-    }
-    next();
-};
-const multer = require('multer');
-// 设置存储引擎
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, __dirname + '/../public/pan/resource/');
-    },
-    filename: function (req, file, cb) {
-        let originalFileName = file.originalname;
-        const currentDate = new Date();
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-        const day = String(currentDate.getDate()).padStart(2, '0');
-        const datePrefix = `${year}${month}${day}`;
-        // 将文件名从可能的乱码编码转换为 UTF-8
-        let decodedFileName = iconv.decode(Buffer.from(originalFileName, 'binary'), 'utf8');
-        cb(null, `${datePrefix}_${decodedFileName}`);
-    }
-});
-const upload = multer({ storage: storage });
+const axios = require('axios');
+const { refreshData, db } = require('../lowdb/pan.js');
 
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.render('pan', pan);
+    let data = db.get('content').value()
+    res.render('pan', { content: data });
 });
-router.post('/upload', authenticate, upload.single('file'), function (req, res, next) {
-    res.send('文件上传成功！');
-    //响应成功后，更改json数据
-    setTimeout(() => {
-        refreshData();
-    }, 1);
+router.post('/', async function (req, res, next) {
+    let { fileName, fileType, fileUrl, fileUser } = req.body;
+    // console.log("req.body:", req.body);
+    //身份验证
+    if (fileUser !== 'sxq') {
+        return res.status(401).send('身份验证失败，文件已删除。');
+    }
+    //操作文件
+    if (fileType === 'add') {
+        //添加文件
+        let data = {
+            name: fileName,
+            path: 'http://www.yydsa.top:6400/parser?url=' + fileUrl,
+            jsonPath: 'http://www.yydsa.top:6400/json/parser?url=' + fileUrl
+        }
+        db.get('content').unshift(data).write()
+        console.log('添加文件成功！:', fileName)
+        //URL是否合法
+        refreshData(data)
+    } else if (fileType === 'sub') {
+        //删除文件
+        db.get('content').remove({ name: fileName }).write()
+        console.log('删除文件成功！:', fileName)
+    }
+
+    res.send('操作成功！');
+
 });
 
 module.exports = router;
